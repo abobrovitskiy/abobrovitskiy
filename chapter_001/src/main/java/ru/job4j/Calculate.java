c8a332a7c9d51d1a578fa41a49ee28cd2e0aea14
abobrovitsky <a.bobrovitsky@gmail.com>
2017-09-23 17:28:25 +0300

    checkstyle is OK [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,17 @@
 package ru.job4j;
 /**
-* Класс Calculate для вывода на экран приветствия
+* Класс Calculate для вывода на экран приветствия.
 * @author abobrovitskiy
 */
 public class Calculate {
-	
-	public static void main (String[] args){
+	/**
+	* Main Method.
+	* @param args args.
+	*/
+	public static void main(String[] args) {
+		/**
+		* Console out Hello World.
+		*/
 		System.out.println("Hello World!");
 	}
 }
--- chapter_001/src/main/java/ru/job4j/package-info.java
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -1,4 +1,4 @@
 /**
 * Calculator.
 */
-package ru.job4j
+package ru.job4j;
