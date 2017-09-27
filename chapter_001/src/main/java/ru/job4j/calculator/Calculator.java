17455406b5bf7ac8821de233bf3887aad37fc372
abobrovitsky <a.bobrovitsky@gmail.com>
2017-09-27 22:00:46 +0300

    Added checkstyle comment

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,49 @@
 package ru.job4j.calculator;
-
+/**
+* Класс Calculator - простые арифметические операции.
+* @author abobrovitskiy.
+*/
 public class Calculator {
-	private double result;
-	
-public void add(double first, double second){
+	/**
+	* result переменная типа double.
+	*/
+private double result;
+	/**
+	* метод add - сложение двух чисел.
+	* @param first first.
+	* @param second second.
+	*/
+public void add(double first, double second) {
 	this.result = first + second;
 }
-
+	/**
+	* метод subtract - вычитание одного числа из другого.
+	* @param first first.
+	* @param second second.
+	*/
 public void subtract(double first, double second) {
 	this.result = first - second;
 }
-
-public void div(double first, double second); {
+	/**
+	* метод div - деление чисел.
+	* @param first first.
+	* @param second second.
+	*/
+public void div(double first, double second) {
 	this.result = first / second;
 }
-
-public void multiple(double first, double second); {
-	this.result = first*second;
+	/**
+	* метод multiple - сложение двух чисел.
+	* @param first first.
+	* @param second second.
+	*/
+public void multiple(double first, double second) {
+	this.result = first * second;
 }
-
+	/**
+	* метод getResult возвращает значение переменной result.
+	* @return result result.
+	*/
 public double getResult(); {
 	return this.result;
 }
