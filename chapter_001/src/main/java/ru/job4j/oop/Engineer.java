28a46e716c135b17fb34fa7acf4e42375396b2d7
abobrovitsky <a.bobrovitsky@gmail.com>
2017-10-20 10:28:21 +0300

    Added task [#6837]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Engineer.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Engineer extends Profession {
+	Child child;
+	
+	public void fix(Car car) {
+		
+	}
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/Teacher.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Teacher extends Profession {
+	Wife wife;
+	
+	public void teach(Child child) {
+		
+	}
+}
