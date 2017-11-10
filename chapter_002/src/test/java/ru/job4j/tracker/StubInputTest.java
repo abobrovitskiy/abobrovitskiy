b8b7f020d05a07b6b735044e68f5afecc006ab5f
abobrovitsky <a.bobrovitsky@gmail.com>
2017-11-10 14:41:31 +0300

    Some tests [#785]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/StubInputTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+/**
+* StubInputTest класс для тестирования метода StubInput.
+*/
+
+public class StubInputTest {
+	@Test
+	public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+		Tracker tracker = new Tracker();
+		Input input = new StubInput(new String[]{"0", "test name", "desc", "123", "6"});
+		new StartUI(input, tracker).init();
+		assertThat(tracker.findAll()[0].getName(), is("test name"));
+	}
+	@Test
+	public void whenUpdateThenTrackerHasUpdatedValue() {
+		Tracker tracker = new Tracker();
+		Item item = tracker.add(new Item("test1", "testDescription", 123L));
+		System.out.println(tracker.findById(item.getId()).getName());
+		Input input = new StubInput(new String[]{"2", "test", "desc", "123", "6"});
+		new StartUI(input, tracker).init();
+		assertThat(tracker.findById(item.getId()).getName(), is("test"));
+	}
+}
