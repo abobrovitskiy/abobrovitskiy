99b0897cbcc77d4452b73f80ffb825982d722ed8
abobrovitsky <a.bobrovitsky@gmail.com>
2017-12-01 11:00:35 +0300

    Added task [#787]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -0,0 +1,112 @@
+package ru.job4j.tracker;
+
+public class MenuTracker {
+	private Input input;
+	private Tracker tracker;
+	private UserAction[] actions = new UserAction[6];
+	public MenuTracker(Input input, Tracker tracker) {
+		this.input = input;
+		this.tracker = tracker;
+	}
+	public void fillActions() {
+		this.actions[0] = new AddItem();
+		this.actions[1] = new ShowAllItems();
+		this.actions[2] = new UpdateItem();
+		this.actions[3] = new DeleteItem();
+		this.actions[4] = new FindItemById();
+		this.actions[5] = new FindItemByName();
+	}
+	public void select(int key) {
+		this.actions[key].execute(this.input, this.tracker);
+	}
+	public void show() {
+		for (UserAction action : this.actions) {
+			if (action != null) {
+				System.out.println(action.info());
+			}
+		}
+	}
+	private static class AddItem implements UserAction {
+		public int key() {
+			return 0;
+		}
+		public  void execute(Input input, Tracker tracker) {
+			String name = input.ask("Please enter the task name: ");
+			String desc = input.ask("Please enter description: ");
+			long create = Long.parseLong(input.ask("Please enter create: "));
+			tracker.add(new Item(name, desc, create));
+		}
+		public String info() {
+			return String.format("%s. %s", this.key(), "Add the new item");
+		}
+	}
+	private static class ShowAllItems implements UserAction {
+		public int key() {
+			return 1;
+		}
+		public  void execute(Input input, Tracker tracker) {
+			for (Item item : tracker.findAll()) {
+				System.out.println(String.format("%s. %s", item.getId(), item.getName())
+				);
+			}
+		}
+		public String info() {
+			return String.format("%s. %s", this.key(), "Show all items");
+		}
+	}
+	private static class UpdateItem implements UserAction {
+		public int key() {
+			return 2;
+		}
+		public  void execute(Input input, Tracker tracker) {
+			String id = input.ask("Enter item id: ");
+			String name = input.ask("Enter new name: ");
+			String desc = input.ask("Enter new description: ");
+			String creat = input.ask("Enter new create: ");
+			long create = Long.parseLong(creat);
+			Item item = new Item(name, desc, create);
+			item.setId(id);
+			tracker.update(item);
+		}
+		public String info() {
+			return String.format("%s. %s", this.key(), "Update item");
+		}
+	}
+	private static class DeleteItem implements UserAction {
+		public int key() {
+			return 3;
+		}
+		public  void execute(Input input, Tracker tracker) {
+			String name = input.ask("Enter name: ");
+			tracker.delete(tracker.findById((tracker.findByName(name).getId())));
+			System.out.println("Done!");
+		}
+		public String info() {
+			return String.format("%s. %s", this.key(), "Delete item");
+		}
+	}
+	private static class FindItemById implements UserAction {
+		public int key() {
+			return 4;
+		}
+		public  void execute(Input input, Tracker tracker) {
+			String id = input.ask("Enter id: ");
+			System.out.println(tracker.findById(id));
+		}
+		public String info() {
+			return String.format("%s. %s", this.key(), "Find item by Id");
+		}
+	}
+	private static class FindItemByName implements UserAction {
+		public int key() {
+			return 5;
+		}
+		public  void execute(Input input, Tracker tracker) {
+			String name = input.ask("Enter name: ");
+			System.out.println(tracker.findByName(name));
+		}
+		public String info() {
+			return String.format("%s. %s", this.key(), "Find item by Name");
+		}
+	}
+}
--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 /**
 * класс StartUI реализует интерфейс программы.
 * @author abobrovitskiy.
@@ -9,96 +8,22 @@ import java.io.InputStreamReader;
 public class StartUI {
 	private Input input;
 	public Tracker tracker;
-	private static final int ADD = 0;
-	private static final int ALL = 1;
-	private static final int UPD = 2;
-	private static final int DEL = 3;
-	private static final int FID = 4;
-	private static final int FNM = 5;
-	private static final int EXIT = 6;
-	private boolean isExit = false;
 	public StartUI(Input input, Tracker tracker) {
 		this.input = input;
 		this.tracker = tracker;
 	}
-	public void showMenu() {
-		System.out.println("0. Add new Item");
-		System.out.println("1. Show all items");
-		System.out.println("2. Update item");
-		System.out.println("3. Delete item");
-		System.out.println("4. Find item by Id");
-		System.out.println("5. Find items by name");
-		System.out.println("6. Exit Program");
-	}
-	public void addItem() {
-		String name = input.ask("Enter name: ");
-		String desc = input.ask("Enter description: ");
-		String creat = input.ask("Enter create: ");
-		long create = Long.parseLong(creat);
-		Item item = new Item(name, desc, create);
-		tracker.add(item);
-	}
-	public void updItem() {
-		String id = input.ask("Enter item id: ");
-		String name = input.ask("Enter new name: ");
-		String desc = input.ask("Enter new description: ");
-		String creat = input.ask("Enter new create: ");
-		long create = Long.parseLong(creat);
-		Item item = new Item(name, desc, create);
-		item.setId(id);
-		tracker.update(item);
-		System.out.println("Updated");
-	}
-	public void delItem() {
-		String name = input.ask("Enter name: ");
-		tracker.delete(tracker.findById((tracker.findByName(name).getId())));
-		System.out.println("Deleted");
-	}
-	public void findById() {
-		String id = input.ask("Enter id: ");
-		System.out.println(tracker.findById(id));
-	}
-	public void showAll() {
-		for (Item item : tracker.findAll()) {
-			System.out.println(item);
-		}
-	}
-	public void findByName() {
-		String name = input.ask("Enter name: ");
-		System.out.println(tracker.findByName(name));
-	}
 	public void init() {
-		while (!isExit) {
-			this.showMenu();
-			String line = input.ask("Select: ");
-			int choice = Integer.parseInt(line);
-			if (ADD == choice) {
-				this.addItem();
-			}
-			if (ALL == choice) {
-				this.showAll();
-			}
-			if (UPD == choice) {
-				this.updItem();
-			}
-			if (DEL == choice) {
-				this.delItem();
-			}
-			if (FID == choice) {
-				this.findById();
-			}
-			if (FNM == choice) {
-				this.findByName();
-			}
-			if (EXIT == choice) {
-				System.out.print("Exiting...");
-				isExit = true;
-				break;
-			}
-		}
+		MenuTracker menu = new MenuTracker(this.input, tracker);
+		menu.fillActions();
+		do {
+			menu.show();
+			int key = Integer.valueOf(input.ask("Select: "));
+			menu.select(key);
+		} while(! "y".equals(this.input.ask("Exit? y")));
 }
 	public static void main(String[] args) throws IOException {
 		Tracker tracker = new Tracker();
-		new StartUI(new ConsoleInput(), tracker).init();
+		Input input = new ConsoleInput();
+		new StartUI(input, tracker).init();
 	}
 }
